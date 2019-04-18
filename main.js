eb36315dd2b4ae92309407b8ef82afda89201fba
admin <admin@admins-MacBook-Pro.local>
2019-04-18 11:02:50 -0500

    updated modals with sized pics and new map

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function createModal(input) {
     var coffeeOrigin = document.getElementById('coffeeOrigin');
     var tastingNotes = document.getElementById('tastingNotes');
     var imgDiv = document.getElementById('imgDiv');
+    var mapContainer = document.getElementById('coffeeButton');
     var inputid = input.id;
     console.log(input);
     coffees.forEach(function(coffee){
@@ -23,6 +24,8 @@ function createModal(input) {
         }
     })
 }
+
+
 /*
 
 function coffeeData() {
@@ -45,7 +48,8 @@ var coffees = [
         roast: 'light',
         origin: 'Ethiopia',
         taste: 'Taste notes: Milk chocolate, apricot jam, dulce de leche, passion fruit, & pumpkin pie spice. Creamy, velvety mouthfeel with medium body & an approachable white peach malic acidity. Hints of apple & pineapple juice on the linger.',
-        img: 'img/lightcity.jpg'
+        img: 'img/lightcity.jpg',
+        center: [38.73694, 9.02722],
 
     },
     {id: 2, name: 'Half City'
@@ -53,78 +57,91 @@ var coffees = [
         origin: 'Ethiopia',
         taste: 'On the nose: citrus, sweet sugared pineapple. Taste: Meyer lemon, fresh red currant, cane sugar, & hints of pineapple juice. Delicate & Fruity. A stunning flavor profile you\'ll absolutely love in any preparation.',
         img:'img/halfcity.jpg',
+        center: [38.73694, 9.02722],
     },
     {id: 3, name: 'Cinnamon',
         roast: 'light',
         origin: 'Indonesia',
         taste: 'Taste notes: beautiful & unique coffee with red currant acidity atop a smooth bakers chocolate base.',
-        img:'img/cinnamon.jpeg',
+        img:'img/cinnamon.jpg',
+        center: [98.71, 2.941],
     },
     {id: 4, name: 'City',
         roast: 'medium',
         origin: 'Guatemala',
         taste: 'Taste: Rich creamy milk chocolate, sweet fruit notes of strawberry, blackberry, golden raisin, mandarin orange, & slight rum as it cools.',
-        img:'img/city.jpeg',
+        img:'img/city.jpg',
+        center: [-90.73333, 14.56667],
     },
     {id: 5, name: 'American',
         roast: 'medium',
         origin: 'Hawaii',
         taste: 'Complex, multi-layered, spice-toned. Coconut, blackberry, ginger blossom, wild honey, dark chocolate in aroma and cup. The structure is sweet, tart and savory in balance with juicy, bright acidity; plush, syrupy mouthfeel.',
         img:'img/america.jpg',
+        center: [-155.97789, 19.72202],
     },
     {id: 6, name: 'Breakfast',
         roast: 'medium',
         origin: 'Costa Rica',
         taste: 'Brown sugar, lavender, blueberry, dark rum, banana liqueur, strawberry, hints of marmalade, grape juice, & almond biscotti.',
         img:'img/bfast.jpg',
+        center: [-83.7534, 9.7489],
     },
     {id: 7, name: 'High',
         roast: 'dark',
         origin: 'Kenya',
         taste: 'Almond brittle, Fuji apple, creamy Dulce de Leche, milk chocolate, red grape, fruit tones of apricot & pineapple. Overall rich and round with crisp malic acidity.',
         img:'img/high.jpg',
+        center: [36.81667, -1.28333],
     },
     {id: 8, name: 'Continental',
         roast: 'dark',
         origin: 'Latin America',
         taste: 'Spiced apple pie, plum pudding, almond brittle, maple syrup, & allspice. Great medium body complemented with a silky Red Anjou pear acidity coming through. This pulped natural process coffee is truly exquisite.',
-        img:'img/continental.jpeg',
+        img:'img/continental.jpg',
+        center: [-90.73333, 14.56667],
     },
     {id: 9, name: 'New Orleans',
         roast: 'dark',
         origin: 'New Orleans',
         taste: 'Spiced apple pie, plum pudding, almond brittle, maple syrup, & allspice. Great medium body complemented with a silky Red Anjou pear acidity coming through. This pulped natural process coffee is truly exquisite.',
-        img:'img/neworleans.jpeg',
+        img:'img/neworleans.jpg',
+        center: [-89.93, 30.04],
     },
     {id: 10, name: 'European',
         roast: 'dark',
         origin: 'Netherlands',
         taste: 'Taste notes: beautiful & unique coffee with red currant acidity atop a smooth bakers chocolate base.',
         img:'img/euro.jpg',
+        center: [4.3007, 52.0705],
     },
     {id: 11, name: 'Espresso',
         roast: 'dark',
         origin: 'Global',
         taste: 'Taste notes: Milk chocolate, apricot jam, dulce de leche, passion fruit, & pumpkin pie spice. Creamy, velvety mouthfeel with medium body & an approachable white peach malic acidity. Hints of apple & pineapple juice on the linger.',
         img:'img/espresso.png',
+        center: [-75.1216, 20.0117],
     },
     {id: 12, name: 'Viennese',
         roast: 'dark',
         origin: 'Italy',
         taste: 'Almond brittle, Fuji apple, creamy Dulce de Leche, milk chocolate, red grape, fruit tones of apricot & pineapple. Overall rich and round with crisp malic acidity.',
         img:'img/vienna.png',
+        center: [11.49, 43.06],
     },
     {id: 13, name: 'Italian',
         roast: 'dark',
         origin: 'Italy',
         taste: 'Almond brittle, Fuji apple, creamy Dulce de Leche, milk chocolate, red grape, fruit tones of apricot & pineapple. Overall rich and round with crisp malic acidity.',
         img:'img/italian.jpg',
+        center: [11.49, 43.06],
     },
     {id: 14, name: 'French',
         roast: 'dark',
         origin: 'France',
         taste: 'On the nose: citrus, sweet sugared pineapple. Taste: Meyer lemon, fresh red currant, cane sugar, & hints of pineapple juice. Delicate & Fruity. A stunning flavor profile you\'ll absolutely love in any preparation.',
-        img:'img/french.jpeg',
+        img:'img/french.jpg',
+        center: [1.688333, 45.436105],
     },
 ];
 
@@ -234,3 +251,21 @@ function makeModal() {
     }
 }
 */
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGJ1dGxlcjI0OTkiLCJhIjoiY2p1anY0M3JzMDF1NDQ0czEzaGdqMXpociJ9.fP70VfqbTfe1PpT348UXQw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 9,
+    center: [-98.4870, 29.4284]
+});
+
+document.getElementById('chipotleButton').addEventListener('click', function () {
+    map.flyTo({
+        center: [-95.58673, 29.73733],
+        zoom: 17
+    });
+});
+
+
+
